619477b313218e7e920bbe55f6535c0572c73f22
dimaslz <dimaslopezzurita@gmail.com>
2016-02-11 01:34:51 +0100

    Update server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var config = require('./config');
 
 var app = express();
 
-// app.use(express.static('./public'));
+app.use(express.static('./public'));
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
